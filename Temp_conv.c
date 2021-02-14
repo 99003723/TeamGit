f653064f3fb21625b217b215944fb7fee206fc2e
99003723 <78869361+99003723@users.noreply.github.com>
2021-02-14 21:21:38 +0530

    Temp conversion added

---
+++ Temp_conv.c
@@ -0,0 +1,15 @@
+void main()
+
+{
+
+    float c,f;
+
+    printf("Enter the temperature in Celcius: ");
+
+    scanf("%f",&c);
+
+    f = c * 9/5 + 32;
+
+    printf("Temperature in Farenheit: %f",f);
+
+}
